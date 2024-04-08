e6ce73f0b8051381e684cc54b926ec512ba48f7d
jkronerffm <mail@joerg-kroner.de>
2024-04-08 12:57:21 +0200

    moved conversion of hexcolor to hexcolorss.py

--- common/AnsiColors.py
+++ common/AnsiColors.py
@@ -35,19 +35,5 @@ class Colors:
     def __call__(self, color : Color, background : BGColor = BGColor.Black, weight: Weight=Weight.Normal):
         return f"{Colors._initial}[{color.value};{background.value};{weight.value}m"
 
-def hexToRgb(hx):
-    return (int(hx[1:3],16), int(hx[3:5],16), int(hx[5:7],16))
-    
-def rgbTupleToHex(colortuple):
-    return "#" + ''.join(f'{i:02X}' for i in colortuple)
-    
-def rgbToHex(r, g, b):
-    return "#{:02x}{:02x}{:02x}".format(r, g, b)
-    
-if __name__ == "__main__":
-    logging.basicConfig(level="DEBUG")
-    s = rgbTupleToHex((128,0,0))
-    logging.debug(f"color string: {s}")
-    t = hexToRgb(s)
-    logging.debug(f"color tuple: {t}")
+
     
