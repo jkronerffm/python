4786cbb13745afa4abd76cdd05d09df651ae9a20
jkronerffm <mail@joerg-kroner.de>
2024-04-17 12:24:44 +0200

    define Clock object

--- radio2/Clock.py
+++ radio2/Clock.py
@@ -29,8 +29,12 @@ class Date(Text):
     def draw(self, surface):
         super().draw(surface)
 
+    def onMouseDown(self, pos, button):
+        self.debug(f"(pos={pos}, button={button})")
+        return True
+    
 class Clock(GraphObjectGroup):
-    def __init__(self, size=Size((800, 400)), pos = Point((0,0)), orientation = Orientation.TopLeft, active=True):
+    def __init__(self, size = None, pos = Point((0, 0)), orientation = Orientation.TopLeft, active=True):
         logging.debug(f"Clock.__init__(size={size}, pos={pos})")
         super().__init__(pos=pos, size=size, orientation=orientation, backgroundColor = Colors.Black, active = active)
         self.addGraphObject(Date())
@@ -40,3 +44,15 @@ class Clock(GraphObjectGroup):
         logging.debug(f"{self.__class__.__name__}.onMouseDown(pos={pos}, button={button})")
         return True
         
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    try:
+        runner = PGRunner(Size((1024, 768)), Colors.Black)
+        runner.addGraphObject(Clock(size = Size((800, 400)), pos=Point(tuple(runner.size()/2)), orientation = Orientation.Center))
+        runner.run()
+    except Exception as e:
+        logging.exception(f"caught exception: {e}")
+    finally:
+        pygame.quit()
+        sys.exit()
+    
