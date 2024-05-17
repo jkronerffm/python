7d0494f109f5e91856350741f07771a8ebb15907
jkronerffm <mail@joerg-kroner.de>
2024-05-17 15:01:15 +0200

    enable multiple pics at one sprite

--- weather/Sprite.py
+++ weather/Sprite.py
@@ -6,25 +6,51 @@ import pygame
 
 class Sprite:
     FileSelector = FileSelector(os.path.join(os.getcwd(), 'images'), ['png', 'jpg', 'svc'])
-    def __init__(self, imagename, pos = (0, 0), size = None):
-        imagepath = Sprite.FileSelector.select(imagename)
-        image = pygame.image.load(imagepath)
-        if size == None:
-            self._image = image
-        else:
-            self._image = pygame.transform.scale(image, size)
+    def __init__(self, imagenames, pos = (0, 0), size = None):
+        self._images = {}
+        for imagename in imagenames:
+            imagepath = Sprite.FileSelector.select(imagename)
+            image = pygame.image.load(imagepath)
+            if size == None:
+                self._images[imagename] = image
+            else:
+                self._images[imagename] = pygame.transform.scale(image, size)
         self._pos = pos
         self._size = size
-        pass
+        self._currentImage = 0
 
+    @property
+    def currentImage(self):
+        return self._currentImage
+
+    @currentImage.setter
+    def currentImage(self, value):
+        self._currentImage = value
+
+    @property
+    def images(self):
+        return self._images
+    
+    @property
+    def image(self):
+        return self.images[self.currentImage]
+
+    @property
+    def width(self):
+        return self.image.get_width()
+
+    @property
+    def height(self):
+        return self.image.get_height()
+    
     def update(self):
         pass
 
     def draw(self, screen):
-        rx = self._image.get_width() / 2
-        ry = self._image.get_height() / 2
+        rx = self.width / 2
+        ry = self.height / 2
         pos = (self._pos[0] - rx, self._pos[1] - ry)
-        screen.blit(self._image, list(pos))
+        screen.blit(self.image, list(pos))
         
     def set_left(self, value):
         self._pos[0] = value
